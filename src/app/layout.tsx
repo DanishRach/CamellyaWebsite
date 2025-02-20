9d0c2a7cb0384142b7804642411723abb235bb56
Danish As-Salafy Rachman <129130056+DanishRach@users.noreply.github.com>
2025-02-20 11:19:46 +0700

    add icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,19 +4,17 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Camellya",
   description: "",
-  icons: {
-    icon: "/icon.ico", 
-  },
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <head></head>
+      <head>
+        <link rel="icon" href="/icon.ico?v=2" type="image/x-icon" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icon.ico?v=2" />
+      </head>
       <body className={`antialiased`}>{children}</body>
     </html>
   );
