7085afa30ef3ffa3cc825f369718713b0a8631c1
Danish As-Salafy Rachman <129130056+DanishRach@users.noreply.github.com>
2025-02-20 11:33:15 +0700

    add thumbail

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,25 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Camellya",
-  description: "",
+  description: "My 'Waifu' website",
+  icons: {
+    icon: "/icon.ico?v=2",
+    apple: "/icon.ico?v=2",
+  },
+  openGraph: {
+    title: "Camellya",
+    description: "A dedicated website for my waifu ❤️.",
+    url: "https://camellya-website.vercel.app/", // Sesuaikan dengan URL website
+    siteName: "Camellya",
+    images: [
+      {
+        url: "https://camellya-website.vercel.app/1.jpg", // Gantilah dengan path gambar yang benar
+        width: 1200,
+        height: 630,
+        alt: "Camellya - My Waifu",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
