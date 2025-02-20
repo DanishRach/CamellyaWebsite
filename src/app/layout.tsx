4be97ce7c1e05944718e1727471ef60d61faab3d
Danish As-Salafy Rachman <129130056+DanishRach@users.noreply.github.com>
2025-02-20 11:14:56 +0700

    add icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Danish Portofolio",
-  description: "All My Projects",
+  title: "Camellya",
+  description: "",
   icons: {
     icon: "/icon.ico", 
   },
